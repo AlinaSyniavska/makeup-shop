7fc0f316dd8ec6b65cc491571feb889f1fa7fbb0
AlinaSyniavska <alina22syniavska@gmail.com>
2022-08-14 23:28:17 +0300

    fix Product Schema

--- dataBase/product.js
+++ dataBase/product.js
@@ -2,69 +2,74 @@ const {Schema, model} = require('mongoose');
 const {priceSignEnum} = require("../constants");
 
 const ProductSchema = new Schema({
-    brand: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    name: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    price: {
-        type: Number,
-        trim: true,
-        required: true,
-    },
-    priceSign: {
-        type: String,
-        trim: true,
-        enum: Object.values(priceSignEnum),
-        required: true,
-    },
-    imageLink: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    rating: {
-        type: String | Number,
-    },
-    category: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    productType: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    tagList: {
-        type: [String],
-    },
-    productColors: [
-        {
-            hexValue: {
+            brand: {
                 type: String,
                 trim: true,
+                required: true,
             },
-            colorName: {
+            name: {
                 type: String,
                 trim: true,
+                required: true,
             },
+            price: {
+                type: Number,
+                trim: true,
+                required: true,
+            },
+            priceSign: {
+                type: String,
+                trim: true,
+                enum: Object.values(priceSignEnum),
+                required: true,
+            },
+            total: {
+                type: Number,
+                trim: true,
+                required: true,
+            },
+            imageLink: {
+                type: String,
+                trim: true,
+                required: true,
+            },
+            description: {
+                type: String,
+                required: true,
+            },
+            rating: {
+                type: String,
+            },
+            category: {
+                type: String,
+                trim: true,
+                required: true,
+            },
+            productType: {
+                type: String,
+                trim: true,
+                required: true,
+            },
+            tagList: {
+                type: [String],
+            },
+            // productColors: [
+            //     {
+            //         hexValue: {
+            //             type: String,
+            //             trim: true,
+            //         },
+            //         colorName: {
+            //             type: String,
+            //             trim: true,
+            //         },
+            //     },
+            // ],
         },
-    ],
-},
-{
-    timestamps: true
-}
-)
+        {
+            timestamps: true
+        }
+    )
 ;
 
 module.exports = model('product', ProductSchema);
--- validators/admin/admin.product.validator.js
+++ validators/admin/admin.product.validator.js
@@ -9,16 +9,17 @@ module.exports = {
         brand: commonDataValidator.required(),
         price: Joi.number().required(),
         priceSign: Joi.string().default(Object.values(priceSignEnum.UAN)).max(3).valid(...Object.values(priceSignEnum)).required(),
+        total: Joi.number().required(),
         imageLink: Joi.string().required(),
         description: Joi.string().default('Some description about product'),
-        rating: Joi.string().alphanum(),
+        rating: Joi.string(),
         category: commonDataValidator.required(),
         productType: commonDataValidator.required(),
         tagList: Joi.array().items(Joi.string().valid(...tagEnum.FILTER_TAGS)),
-        productColors: Joi.array().items({
-            hexValue: Joi.string().regex(/^#[A-Fa-f\d]{6}/),
-            colorName: Joi.string(),
-        })
+        // productColors: Joi.array().items({
+        //     hexValue: Joi.string().regex(/^#[A-Fa-f\d]{6}/),
+        //     colorName: Joi.string(),
+        // })
     }),
 
     updateProductValidator: Joi.object({
@@ -26,15 +27,16 @@ module.exports = {
         brand: commonDataValidator,
         price: Joi.number(),
         priceSign: Joi.string().max(3).valid(...Object.values(priceSignEnum)),
+        total: Joi.number(),
         imageLink: Joi.string(),
         description: Joi.string(),
-        rating: Joi.string().alphanum(),
+        rating: Joi.string(),
         category: commonDataValidator,
         productType: commonDataValidator,
         tagList: Joi.array().items(Joi.string().valid(...tagEnum.FILTER_TAGS)),
-        productColors: Joi.array().items({
+/*        productColors: Joi.array().items({
             hexValue: Joi.string().regex(/^#[A-Fa-f\d]{6}/),
             colorName: Joi.string(),
-        })
+        })*/
     }),
 };
