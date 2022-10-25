0b9c4584e3c4d0e2813f5bac74d48217da2d40f2
AlinaSyniavska <alina22syniavska@gmail.com>
2022-10-25 10:50:48 +0300

    add to homeRouter /product/:id

--- routes/home/home.router.js
+++ routes/home/home.router.js
@@ -1,11 +1,16 @@
 const homeRouter = require('express').Router();
 
 const {productController} = require("../../controllers");
-const {commonMiddleware} = require("../../middlewares");
+const {commonMiddleware, adminCommonMiddleware} = require("../../middlewares");
 const {queryValidator} = require("../../validators");
+const {Product} = require("../../dataBase");
 //  /home?sortOrder=5
 homeRouter.get('/',
     commonMiddleware.isDataValid(queryValidator.allProductsValidator, 'query'),
     productController.getAllProducts);
+homeRouter.get('/product/:id',
+    commonMiddleware.isIdValid,
+    adminCommonMiddleware.isItemPresent(Product),
+    productController.getProductById);
 
 module.exports = homeRouter;
